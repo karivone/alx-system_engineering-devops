cb66eb7edea0b8a33dca1e47f752fd42a0cad484
karvone <karimiyvonne19@gmail.com>
2023-04-24 21:25:38 +0300

    export to CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 '''
 Gather data from an API
 Export data in the CSV format
@@ -11,21 +12,12 @@ from sys import argv
 if __name__ == "__main__":
     user_id = argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(f"{url}users/{user_id}").json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    todos = requests.get(f"{url}todos", params={"userId": user_id}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open(f"{user_id}.csv", "w", newline="") as csvfile:
-        fieldnames = ["userId", "username", "completed", "title"]
-        writer = csv.DictWriter(csvfile, quoting=csv.QUOTE_ALL)
-        writer.writeheader()
-        task_data = [
-                {
-                    "userId": user_id,
-                    "username": username,
-                    "completed": task.get("completed"),
-                    "title": task.get("title")
-                }
-                for task in todos
-        ]
-        writer.writerows(task_data)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([user_id, username,
+                            task.get("completed"), task.get("title")])

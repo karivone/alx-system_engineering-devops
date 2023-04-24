7cef33d1f655999822e0eb0d63f737b36d2bf7c4
karvone <karimiyvonne19@gmail.com>
2023-04-24 19:38:34 +0300

    :gather data from an Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-# Python script that, using this REST API, for a given employee ID, returns
-# information about his/her TODO list progress.
+
+'''Gather data from an API'''
+
 import requests
-import sys
-
-
-def display():
-    # Fetching the employee name
-    user_url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-    try:
-        employee_name = requests.get(user_url).json()['name']
-    except KeyError:
-        print(f"Invalid employee ID: {sys.argv[1]}")
-        return
-
-    # Fetching the employee's todo list
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}"
-    todo_data = requests.get(todo_url).json()
-
-    # Extracting completed tasks and their titles
-    completed_tasks = [task for task in todo_data if task['completed']]
-    num_completed_tasks = len(completed_tasks)
-    task_titles = [task['title'] for task in completed_tasks]
-
-    # Displaying the progress report
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{len(todo_data)}):")
-    print('\n'.join([f"\t{title}" for title in task_titles]))
-
+from sys import argv
 
 if __name__ == "__main__":
-    display()
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    done_tasks = []
+    for task in todos:
+        if task.get("completed"):
+            done_tasks.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(
+              user.get("name"), len(done_tasks), len(todos)))
+    print("\n".join("\t {}".format(task) for task in done_tasks))

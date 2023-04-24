cbdd70cd84d4ea6446ae0462a337e53a47013dd8
karvone <karimiyvonne19@gmail.com>
2023-04-24 16:48:48 +0300

    gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
 #Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
-
 import requests
 import sys
 
-employee_id = sys.argv[1]
+def display():
+    # Fetching the employee name
+    user_url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+    employee_name = requests.get(user_url).json()['name']
 
-# Fetching the employee name
-employee_url = 'https://jsonplaceholder.typicode.com/users/' + employee_id
-employee_data = requests.get(employee_url).json()
+    # Fetching the employee's todo list
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}"
+    todo_data = requests.get(todo_url).json()
 
-if 'name' in employee_data:
-    employee_name = employee_data['name']
-else:
-    employee_name = 'Unknown'
+    # Extracting completed tasks and their titles
+    completed_tasks = [task for task in todo_data if task['completed']]
+    num_completed_tasks = len(completed_tasks)
+    task_titles = [task['title'] for task in completed_tasks]
 
-# Fetching the employee's todo list
-todo_url = 'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id
-todo_data = requests.get(todo_url).json()
+    # Displaying the progress report
+    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{len(todo_data)}):")
+    print('\n'.join([f"\t{title}" for title in task_titles]))
 
-# Counting the number of completed tasks
-completed_tasks = [todo for todo in todo_data if todo['completed']]
-num_completed_tasks = len(completed_tasks)
-num_total_tasks = len(todo_data)
-
-# Displaying the progress report
-print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{num_total_tasks}):")
-for task in completed_tasks:
-    print(f"\t {task['title']}")
+if __name__ == "__main__":
+    display()
 

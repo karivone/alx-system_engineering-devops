79e777a936a9b9eee38026691246551c53f47a39
karvone <karimiyvonne19@gmail.com>
2023-04-24 17:05:48 +0300

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+#extend your Python script to export data in the JSON format.
+import json
+import requests
+
+if __name__ == '__main__':
+    url_base = 'https://jsonplaceholder.typicode.com'
+    users_url = url_base + '/users'
+    todos_url = url_base + '/todos'
+
+    # Get all users
+    response = requests.get(users_url)
+    users = response.json()
+
+    # Get all todos
+    response = requests.get(todos_url)
+    todos = response.json()
+
+    # Group todos by user
+    todos_by_user = {}
+    for todo in todos:
+        user_id = todo['userId']
+        if user_id not in todos_by_user:
+            todos_by_user[user_id] = []
+        todos_by_user[user_id].append(todo)
+
+    # Create JSON object
+    todo_list = {}
+    for user in users:
+        user_id = user['id']
+        todo_list[user_id] = []
+        for todo in todos_by_user[user_id]:
+            todo_item = {
+                'username': user['username'],
+                'task': todo['title'],
+                'completed': todo['completed']
+            }
+            todo_list[user_id].append(todo_item)
+
+    # Write JSON to file
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(todo_list, file)
+

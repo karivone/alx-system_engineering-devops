c8a7fbde8b36e1337026c145544c6b7d5d34f208
karvone <karimiyvonne19@gmail.com>
2023-05-09 16:10:10 +0300

    check no of subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" a script to retrieve the no of subs on a subreddi"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    '''defines the no of subcribers on sub reddit '''
+
+    # Set custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    # Make a request to the Reddit API
+    response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers, allow_redirects=False)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Get the number of subscribers from the response JSON
+        return response.json()['data']['subscribers']
+    else:
+        # Return 0 if the subreddit is invalid
+        return 0
+

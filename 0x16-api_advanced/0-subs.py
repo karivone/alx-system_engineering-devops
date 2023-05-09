031a14bccd2cdc4de8b282e610de8db160e247df
karvone <karimiyvonne19@gmail.com>
2023-05-09 16:13:30 +0300

    check no of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,15 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     '''defines the no of subcribers on sub reddit '''
 
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     # Set custom User-Agent to avoid Too Many Requests error
     headers = {'User-Agent': 'MyBot/0.0.1'}
     # Make a request to the Reddit API
-    response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     # Check if the request was successful
     if response.status_code == 200:
@@ -19,4 +21,3 @@ def number_of_subscribers(subreddit):
     else:
         # Return 0 if the subreddit is invalid
         return 0
-

55704439dbe79cd812901f44b8c107d84efc10a7
karvone <karimiyvonne19@gmail.com>
2023-05-09 17:06:14 +0300

    find top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts for the given subreddit.
+
+    :param subreddit: A string representing the name of the subreddit.
+    """
+
+    # Set the URL to query for the subreddit's hot posts
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    # Set a custom User-Agent header to avoid a 429 Too Many Requests error
+    headers = {"User-Agent": "My Reddit Client"}
+
+    # Query the subreddit's hot posts from the Reddit API
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # If the query was successful, print the titles of the first 10 hot posts
+    if response.status_code == 200:
+        data = response.json().get("data")
+        for post in data.get("children")[:10]:
+            title = post.get("data").get("title")
+            print(title)
+
+    # If the subreddit is invalid, print None
+    else:
+        print(None)

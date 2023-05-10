a99483c9bd41e0e148dda612bb3810b011cf4768
karvone <karimiyvonne19@gmail.com>
2023-05-10 22:21:09 +0300

    script

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+#puppet script that does replace a file path
+
+$file_path = '/var/www/html/wp-settings.php'
+
+#replace "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i \"s/phpp/php/g\" ${file_path}",
+  path    => ['/bin','/usr/bin']
+}

25dec3a76ff8287c3541d9d439f8e2522bb34354
karvone <karimiyvonne19@gmail.com>
2023-05-11 10:57:17 +0300

    replace line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,7 +4,10 @@ $file_path = '/var/www/html/wp-settings.php'
 
 #replace "phpp" with "php"
 
-exec { 'replace_line':
-  command => "sed -i \"s/phpp/php/g\" ${file_path}",
+exec { 'replace_line-error':
+
+  command => "sed -i 's/phpp/php/g' ${file_path}",
+
   path    => ['/bin','/usr/bin']
+
 }
